ae41e8b1f5b0b8f379e0233485c0c8a8cbeeb711
ken_ellingsen <ken.ellingsen@gmail.com>
2024-08-30 13:33:24 -0500

    Added functionality to display window height

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
   useEffect(() => {
-    const windowResize = () => setWindowWidth(window.innerWidth);
+    const windowResize = () => {
+      setWindowWidth(window.innerWidth)
+      setWindowHeight(window.innerHeight)
+    };
+
     window.addEventListener("resize", windowResize);
   },[]);
 
   return (
     <div className="App">
       <p>Window width: {windowWidth}px</p>
+      <p>Window height: {windowHeight}px</p>
     </div>
   );
 };
